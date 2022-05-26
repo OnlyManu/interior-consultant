0dc72c6c268312bad5ed5fb4879bf19e4854aa3e
emmanuel <blimanuevil@gmail.com>
2022-05-26 23:07:57 +0200

    amelioration de la nav mobile

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+const body=document.querySelector("body");
+const navbar=document.querySelector("nav");
 const burgerIcon=document.getElementById("burger");
 const closeIcon=document.getElementById("close");
-const navbar=document.querySelector("nav");
+
 
 const openCloseMenu=()=>{
     navbar.classList.toggle("open");
+    body.classList.toggle("hide");
 }
 
 burgerIcon.addEventListener("click", openCloseMenu);
